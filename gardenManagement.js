7cce5b88c9b7b34d1a3c58c1b9b37e0e3137ae80
Roy Gitahi <roygitahi0@gmail.com>
2026-04-01 14:23:50 +0300

    Add:gardenManagement.js

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,20 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+//Using if statements to check on temperature
+if (temperature > 80) {
+  console.log("Watering on");
+} else {
+  console.log("Watering off");
+}
+//Using if statements to check on time of day
+if (timeOfDay === "evening" || timeOfDay === "night") {
+  console.log("Lights on");
+}else {
+  console.log("Lights off");
+}
+while (soilMoisture < 40) {
+  console.log("Watering the garden...");
+  soilMoisture += 5;
+}
+console.log("Soil moisture is now at " + soilMoisture + "%");
